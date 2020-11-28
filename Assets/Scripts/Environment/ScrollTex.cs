d0dde1991d65386324d34a8f3bb28a60d3629eef
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-11-28 20:21:19 +0000

    Clouds and Gate
    Added clouds that move, added gate sprite, need the 3 diff variants

---
+++ Assets/Scripts/Environment/ScrollTex.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTex : MonoBehaviour
+{
+    float scrollX = 0.01f;
+    float offsetX;
+
+    Renderer r;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        offsetX = r.material.mainTextureOffset.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        offsetX += Time.deltaTime * scrollX;
+        r.material.mainTextureOffset = new Vector2(offsetX, 0);
+        
+    }
+}
