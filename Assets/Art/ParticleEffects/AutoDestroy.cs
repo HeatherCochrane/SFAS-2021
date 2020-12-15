72f62a39f981332d48846cad1e9870b78225bb88
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-12-15 15:58:44 +0000

    Grass Particles

---
+++ Assets/Art/ParticleEffects/AutoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    ParticleSystem p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        p = GetComponent<ParticleSystem>();
+        Invoke("destroyParticles", 1.5f);
+    }
+
+
+    void destroyParticles()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Art/ParticleEffects/GrassEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassEffect : MonoBehaviour
+{
+    [SerializeField]
+    GameObject grassParticles;
+
+    GameObject newGrass;
+
+    public void spawnGrass()
+    {
+        newGrass = Instantiate(grassParticles);
+        newGrass.transform.position = Player.instance.transform.position - new Vector3(0, 1);
+    }
+}
